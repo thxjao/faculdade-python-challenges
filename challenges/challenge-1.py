58a76d4ce15b29c462b6dcee9ca0b225041b382f
Joao Oliveira <thxjao.dev@gmail.com>
2021-09-05 17:26:38 -0300

    dcos: comment code + fixes

--- challenges/challenge-1.py
+++ challenges/challenge-1.py
@@ -1,14 +1,13 @@
 # Exercicio 1
 
-answr = int(input('Inserir dados?   0 - Não   1 - Sim   '))
-print(answr)
-if answr == 1:
-    nome = input('Nome do aluno(a): ')
-    nota = input('Nota final: ')
-    notaFloated = float(nota)
+answr = int(input('Inserir dados?   0 - Não   1 - Sim   ')) #Pergunta se a pessoa gostaria de inserir os dados
+if answr == 1: # Verifica se o resultado for = True, e continua coma  operação
+    nome = input('Nome do aluno(a): ') #Capta o nome e isola em uma variável.
+    nota = input('Nota final: ') #Capta a nota no data tipo de STR e isola em uma variável.
+    notaFloated = float(nota) #Transforma a variável de DataType STR para "Number" seja "INT" ou "FLOAT".
     if notaFloated >= 0 and notaFloated < 3:
-        conceito = 'E'
-    elif notaFloated >= 3 and notaFloated < 5:
+        conceito = 'E'                              #A partir desse processo é realizado uma verificação que de acordo com o tamanho da nota retorna uma variável com o conceito.
+    elif notaFloated >= 3 and notaFloated < 5:   
         conceito = 'D'
     elif notaFloated >= 5 and notaFloated < 7:
         conceito = 'C'
@@ -16,4 +15,6 @@ if answr == 1:
         conceito = 'B'
     else:
         conceito = 'A'
-    print('O aluno(a) {} tirou a nota {} e se enquadra no conceito {}'.format(nome, nota, conceito))
+    print('O aluno(a) {} tirou a nota {} e se enquadra no conceito {}'.format(nome, nota, conceito)) #Retorn o Output com as variáveis inseridas no programa.
+else: #Caso a operação seja negada finaliza o processo.
+    print('Encerrando...')
